fd9cab0ba1f1d9a7f71407ae6f7de4dd07212a25
Kthendavid04 <kthendavid04@gmail.com>
2021-03-27 23:48:19 -0500

    finalized project

---
+++ Assets/JS/questions.js
@@ -0,0 +1,93 @@
+console.log("questions.js load is working");
+var questions = [
+  {
+      title:"When a user views a page containing a JavaScript program, which machine actually executes the script?",
+      choices:["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporte offices", "None of the above"],
+      answer: "The User's machine running a Web browser"
+  },
+  {
+      title:"What are variables used for in JavaScript Programs?",
+      choices:["Storing input content","Storing numbers, dates, or other values", "Varying randomly", "Causing highi-school algebra flashbacks"],
+      answer: "Storing numbers, dates, or other values"
+  },
+  {
+      title:"What should appear at the very end of yoru JavaScript?",
+      choices:["<script>", " 'The END' ", "</script>","nothing, it just ends"],
+      answer: "</script>"
+  },
+  {
+      title:"How dos JavaScript store dates in a date object?",
+      choices:["The number of seconds since Netscape's public stock offering", "The number of days since January 1st, 1970", "The number of milliseconds since January 1st, 1970"],
+      answer: "The number of milliseconds since January 1st, 1970"
+  },
+  {
+      title:"what is the correct syntax for referring to an external script called 'lmnop.js'?",
+      choices:["<script href=' lmnop.js'>", "<script name ='lmnop.js'> ", "<script src ='lmnop.js'>", "/script name = 'lmnop.js' "],
+      answer: "<script src ='lmnop.js"
+  },
+  {
+      title:"Javascript entities starts with _______ and ends with ________.",
+      choices:["semicolon, colon", "semicolon, ampersand", "ampersand, colon", "ampersand, semicolon"],
+      answer: "ampersand, semicolon"
+  },
+  {
+      title:"Which of the following describes JavaScript",
+      choices:["a low-level programming language", "an objet-oriented scripting language", "a scripting language precompiled in the browser", "a compiled scripting language"],
+      answer: "an objet-oriented scripting language"
+  },
+  {
+      title:"Which of the following is NOT considered a JavaScript operator?",
+      choices:["this", "new", "delete", "typeof"],
+      answer: "his"
+  },
+  {
+      title:"_____ method evaluates a string of JavaScript code in the context of the specified object",
+      choices:["Parse, ParseInt, ParseFloat, Eval"],
+      answer: "Eval"
+  },
+  {
+      title:"Using _______ statement is how you test for a specific condition",
+      choices:["select", "switch", "if", "for"],
+      answer: "if"
+  },
+  {
+      title:"The _____ method of an Array object adds and /or removes elements from an array.",
+      choices:["Splice", "Shift", "Slice", "Reverse"],
+      answer: "Splice"
+  },
+  {
+      title:"The syntax of a blur method in a button object is____________",
+      choices:["Blur()","Blur(contrast)", "Blur(value)", "Blur(depth)" ],
+      answer: "Blur()"
+  },
+  {
+      title:"Which company developed JavaScript",
+      choices:["Netflix","Napster", "Netscape", "IBM"],
+      answer: "Netscape"
+  },
+  {
+      title:"Inside which HTML element do we put the JavaScript",
+      choices:["<script>", "<head>", "<meta>", "<style>"],
+      answer: "<script>"
+  },
+  {
+      title:"Which of the following is the correct syntax to display, 'Warning! Your learning too much!'?",
+      choices:["alert-box(''Warning! Your learning too much!');", "confirm(''Warning! Your learning too much!');", "alert(''Warning! Your learning too much!');", "msgbox(''Warning! Your learning too much!')"],
+      answer: "alert(''Warning! Your learning too much!');"
+  },
+  {
+      title:"What is the original name of JavaScript?",
+      choices:["LiveScript", "EScript", "Mocha", "Java"],
+      answer: "Mocha"
+  },
+  {
+      title:"JavaScript can be written_________",
+      choices:["directly on the server page", "directly into HTML pages", "directly into the css file", "directly into JS file and included into HTML"],
+      answer: "directly into JS file and included into HTML"
+  },
+  {
+      title:"How to get a particular value using the tagged name?",
+      choices:["getElementbyID()", "getElementsbyTagName()", "GetElTagNm()", "getElementsbyName()"],
+      answer: "getElementsbyTagName()"
+  },
+];
